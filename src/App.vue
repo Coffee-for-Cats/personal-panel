95c7b48c4c68ec9d54502fc7dd2d4a5ed24815ad
Lucas S. Silva <57202598+Coffee-for-Cats@users.noreply.github.com>
2023-12-01 16:20:16 -0300

    watcher and json interpreting

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, defineAsyncComponent, provide } from 'vue';
+import { ref, defineAsyncComponent, provide, watch } from 'vue';
 
 // a section/toolbar for adding new elements in the page.
 import AddNew from './editing/AddNew.vue';
@@ -10,11 +10,11 @@ import EditPanelButtons from './editing/EditPanelButtons.vue'
 import EditModal from './editing/EditModal.vue'
 
 // hardcoded json API.
-const pagePanels = ref([
-  { component: 'Title', content: {text: 'I am Lucas!'}},
-  { component: 'Paragraph', content: {text: "Welcome to my webpage!"}},
-  { component: 'Title', content: {text: 'I am Matheus!'}}
-]);
+const pagePanels = ref();
+(async () => {
+  const response = await fetch('http://localhost:5000/');
+  pagePanels.value = await response.json();
+})();
 
 // hardcoded editing option.
 const editing = true; // if I can edit the page. Should come from server too.
@@ -25,21 +25,23 @@ if (editing) {
   provide('editTarget', editTarget);
 }
 
+watch(pagePanels, () => {
+  for (const panel of pagePanels.value) {
+    // if the component does not exist yet.
+    if (!pagePanels[panel.component]) {
+      console.log(`loading ${panel.component}`);
+      // gives it to vue download.
+      const component = defineAsyncComponent(
+        () => import(`./components/${panel.component}.vue`)
+      )
+      // saves in the hashtable
+      Components[panel.component] = component;
+    }
+  }
+})
+
 // Dynamic components hashmap.
 const Components = {}
-
-for (const panel of pagePanels.value) {
-  // if the component does not exist yet.
-  if (!pagePanels[panel.component]) {
-    console.log(`loading ${panel.component}`);
-    // gives it to vue download.
-    const component = defineAsyncComponent(
-      () => import(`./components/${panel.component}.vue`)
-    )
-    // saves in the hashtable
-    Components[panel.component] = component;
-  }
-}
 
 </script>
 
